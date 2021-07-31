0f5a54311f8ead461448ef0e6971b750686be707
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-07-31 18:45:29 -0300

    repo start

---
+++ src/test/java/br/com/wagnerrmorais/mvc/mudi/MudiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wagnerrmorais.mvc.mudi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MudiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
