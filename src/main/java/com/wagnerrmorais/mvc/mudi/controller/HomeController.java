ef4b88d95d6a78144f5a7be434bfd68d748f2ce9
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-08-04 20:55:44 -0300

    Implementing database connection

--- src/main/java/com/wagnerrmorais/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/controller/HomeController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
@@ -12,16 +15,14 @@ import java.util.List;
 @Controller
 public class HomeController {
 
+    @PersistenceContext
+    private EntityManager entityManager;
+
     @GetMapping("/home")
     public String home(Model model) {
-        Pedido pedido = new Pedido();
-        pedido.setNomeProduto("Batedeira");
-        pedido.setDescricao("Que bate bate");
-        pedido.setValorNegociado(new BigDecimal("200"));
-        pedido.setUrlProduto("http://ae01.alicdn.com/kf/HTB1QRClQkvoK1RjSZPfq6xPKFXau.jpg?width=791&height=812&hash=1603");
-        pedido.setUrlImagem("https://m.media-amazon.com/images/I/81S9qsHCFfL._AC_SY355_.jpg");
-
-        List<Pedido> pedidos = Arrays.asList(pedido);
+        String sql = "select p from Pedido p";
+        Query query = entityManager.createQuery(sql, Pedido.class);
+        List<Pedido> pedidos = query.getResultList();
 
         model.addAttribute("pedidos",pedidos);
         return "home";
