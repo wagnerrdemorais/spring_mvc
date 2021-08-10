9893d0b9fedc85ab3585c648b1bd0ff7335a8636
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-08-10 19:41:40 -0300

    Implementando JpaRepository

--- src/main/java/com/wagnerrmorais/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/controller/HomeController.java
@@ -20,7 +20,7 @@ public class HomeController {
   @GetMapping("/home")
   public String home(Model model) {
 
-    List<Pedido> pedidos = repository.getPedidos();
+    List<Pedido> pedidos = repository.findAll();
 
     model.addAttribute("pedidos", pedidos);
     return "home";
--- src/main/java/com/wagnerrmorais/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/repository/PedidoRepository.java
@@ -1,22 +1,10 @@
 package com.wagnerrmorais.mvc.mudi.repository;
 
 import com.wagnerrmorais.mvc.mudi.model.Pedido;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.persistence.Query;
-import java.util.List;
-
 @Repository
-public class PedidoRepository {
+public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    @PersistenceContext
-    private EntityManager entityManager;
-
-    public List<Pedido> getPedidos() {
-        String sql = "select p from Pedido p";
-        Query query = entityManager.createQuery(sql, Pedido.class);
-        return query.getResultList();
-    }
 }
