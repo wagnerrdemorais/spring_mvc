9896e950c1d7919b202fff40ff2e011db1f166aa
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-08-12 19:50:05 -0300

    Adicionando navegação entre status

--- src/main/java/com/wagnerrmorais/mvc/mudi/controller/HomeController.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/controller/HomeController.java
@@ -1,14 +1,19 @@
 package com.wagnerrmorais.mvc.mudi.controller;
 
 import com.wagnerrmorais.mvc.mudi.model.Pedido;
+import com.wagnerrmorais.mvc.mudi.model.StatusPedido;
 import com.wagnerrmorais.mvc.mudi.repository.PedidoRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping({"/home", "", "/"})
 public class HomeController {
 
   private final PedidoRepository repository;
@@ -17,12 +22,23 @@ public class HomeController {
     this.repository = pedidoRepository;
   }
 
-  @GetMapping({"/home", "", "/"})
+  @GetMapping
   public String home(Model model) {
-
     List<Pedido> pedidos = repository.findAll();
-
     model.addAttribute("pedidos", pedidos);
     return "home";
+  }
+
+  @GetMapping("/{status}")
+  public String porStatus(@PathVariable("status") String status, Model model) {
+    List<Pedido> pedidos = repository.findByStatus(StatusPedido.valueOf(status.toUpperCase()));
+    model.addAttribute("pedidos", pedidos);
+    model.addAttribute("status", status);
+    return "home";
+  }
+
+  @ExceptionHandler(IllegalArgumentException.class)
+  public String onError() {
+    return "redirect:/home";
   }
 }
--- src/main/java/com/wagnerrmorais/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/repository/PedidoRepository.java
@@ -1,10 +1,14 @@
 package com.wagnerrmorais.mvc.mudi.repository;
 
 import com.wagnerrmorais.mvc.mudi.model.Pedido;
+import com.wagnerrmorais.mvc.mudi.model.StatusPedido;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
+    List<Pedido> findByStatus(StatusPedido status);
 }
