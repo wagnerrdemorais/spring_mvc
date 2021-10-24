71539a89907fdb6c28ed5d1f3f1cdeeebf6d35ae
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-10-24 13:50:57 -0300

    adicionando password encoder, redirecionando para home apos login

--- src/main/java/com/wagnerrmorais/mvc/mudi/WebSecurityConfig.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/WebSecurityConfig.java
@@ -1,18 +1,23 @@
 package com.wagnerrmorais.mvc.mudi;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private DataSource dataSource;
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
@@ -21,22 +26,29 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated()
             .and()
                 .formLogin(form -> form
-                        .loginPage("/login")
-                        .permitAll()
-                )
-                .logout(logout -> logout.logoutUrl("/logout")
-                );
+                    .loginPage("/login")
+                    .defaultSuccessUrl("/home", true)
+                    .permitAll())
+                .logout(logout -> logout.logoutUrl("/logout"));
     }
 
-    @Bean
     @Override
-    public UserDetailsService userDetailsService() {
-        UserDetails user =
-                User.withDefaultPasswordEncoder()
-                        .username("joao")
-                        .password("joao")
-                        .roles("ADM")
-                        .build();
-        return new InMemoryUserDetailsManager(user);
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        String encodedPassword = encoder.encode("maria");
+//
+//        UserDetails user =
+//            User.withDefaultPasswordEncoder()
+//                .username("maria")
+//                .password(encodedPassword)
+//                .roles("ADM")
+//                .build();
+//
+//        System.out.println(encodedPassword);
+
+        auth.jdbcAuthentication()
+                .dataSource(dataSource)
+                .passwordEncoder(encoder);
+//                .withUser(user);
     }
 }
