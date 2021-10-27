01ee23031b1834bb94ff4c28fdb208aa56aad444
Wagner Rodakevicz de Morais <wagnerrdemorais@gmail.com>
2021-10-27 08:20:29 -0300

    adicionando oferta controller, PedidosRest com problema no retorno

---
+++ src/main/java/com/wagnerrmorais/mvc/mudi/api/PedidosRest.java
@@ -0,0 +1,30 @@
+package com.wagnerrmorais.mvc.mudi.api;
+
+import com.wagnerrmorais.mvc.mudi.model.Pedido;
+import com.wagnerrmorais.mvc.mudi.model.StatusPedido;
+import com.wagnerrmorais.mvc.mudi.repository.PedidoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/pedidos")
+public class PedidosRest {
+
+    @Autowired
+    private PedidoRepository pedidoRepository;
+
+    @GetMapping("aguardando")
+    public List<Pedido> getPedidosAguardandoOfertas() {
+        Sort sort = Sort.by("id").descending();
+
+        PageRequest pageRequest = PageRequest.of(0, 10, sort);
+
+        return pedidoRepository.findByStatus(StatusPedido.AGUARDANDO, pageRequest);
+    }
+}
--- src/main/java/com/wagnerrmorais/mvc/mudi/repository/PedidoRepository.java
+++ src/main/java/com/wagnerrmorais/mvc/mudi/repository/PedidoRepository.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Repository
 public interface PedidoRepository extends JpaRepository<Pedido, Long> {
 
-    @Cacheable("byStatus")
+    @Cacheable("findByStatus")
     List<Pedido> findByStatus(StatusPedido status, Pageable sort);
 
     @Query("select p from Pedido p join p.user u where u.username = :username")
